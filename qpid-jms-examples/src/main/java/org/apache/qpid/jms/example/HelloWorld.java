f736414d9dc784074370e584dd514576d4ed8d57
Robbie Gemmell <robbie@apache.org>
2019-08-20 18:28:59 +0100

    DELETE-ME: hacky HelloWorld additions for adhoc testing of tracing

--- qpid-jms-examples/src/main/java/org/apache/qpid/jms/example/HelloWorld.java
+++ qpid-jms-examples/src/main/java/org/apache/qpid/jms/example/HelloWorld.java
@@ -20,48 +20,137 @@
  */
 package org.apache.qpid.jms.example;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.ExceptionListener;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
+import org.apache.qpid.jms.JmsConnectionFactory;
+import org.apache.qpid.jms.tracing.JmsTracer;
+import org.apache.qpid.jms.tracing.opentracing.OpenTracingTracerFactory;
+
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+
+// NOTE: Hacky temporary additions to HelloWorld for ad-hoc tracing 'testing'
 public class HelloWorld {
+
+    private static boolean REGISTER_GLOBAL_TRACER = true;
+    private static boolean SET_TRACER_ON_FACTORY = false;
+
+    private static boolean PRODUCER = true;
+    private static boolean CONSUMER = true;
+    private static boolean USE_ONMESSAGE = true;
+
+    private static long TIMEOUT = 5000;
+
     public static void main(String[] args) throws Exception {
+        if(!PRODUCER && !CONSUMER) {
+            throw new IllegalStateException("Not configured for producing or consuming!");
+        }
+
+        if (REGISTER_GLOBAL_TRACER) {
+            String  serviceName = "some-global-tracer"; // Normally set via env variable, hard coding for testing.
+            Configuration configuration = Configuration.fromEnv(serviceName);
+
+            doHackyTestingConfiguration(configuration);
+
+            Tracer tracer = configuration.getTracer();
+            GlobalTracer.registerIfAbsent(tracer);
+        }
+
+        JmsTracer factorySet = null;
+        if (SET_TRACER_ON_FACTORY) {
+            String  serviceName = "some-factory-set-tracer"; // Normally set via env variable, hard coding for testing.
+            Configuration configuration = Configuration.fromEnv(serviceName);
+
+            doHackyTestingConfiguration(configuration);
+
+            Tracer tracer = configuration.getTracer();
+
+            factorySet = OpenTracingTracerFactory.create(tracer);
+        }
+
         try {
             // The configuration for the Qpid InitialContextFactory has been supplied in
             // a jndi.properties file in the classpath, which results in it being picked
             // up automatically by the InitialContext constructor.
             Context context = new InitialContext();
 
-            ConnectionFactory factory = (ConnectionFactory) context.lookup("myFactoryLookup");
+            JmsConnectionFactory factory = (JmsConnectionFactory) context.lookup("myFactoryLookup");
             Destination queue = (Destination) context.lookup("myQueueLookup");
 
-            Connection connection = factory.createConnection(System.getProperty("USER"), System.getProperty("PASSWORD"));
+            if(factorySet != null) {
+                factory.setTracer(factorySet);
+            }
+
+            Connection connection = factory.createConnection(System.getProperty("USER", "guest"), System.getProperty("PASSWORD", "guest"));
             connection.setExceptionListener(new MyExceptionListener());
             connection.start();
 
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-            MessageProducer messageProducer = session.createProducer(queue);
-            MessageConsumer messageConsumer = session.createConsumer(queue);
+            MessageConsumer messageConsumer = null;
+            if(CONSUMER) {
+                messageConsumer = session.createConsumer(queue);
+            }
 
-            TextMessage message = session.createTextMessage("Hello world!");
-            messageProducer.send(message, DeliveryMode.NON_PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
-            TextMessage receivedMessage = (TextMessage) messageConsumer.receive(2000L);
+            MessageProducer messageProducer = null;
+            if(PRODUCER) {
+                messageProducer = session.createProducer(queue);
 
-            if (receivedMessage != null) {
-                System.out.println(receivedMessage.getText());
-            } else {
-                System.out.println("No message received within the given timeout!");
+                TextMessage message = session.createTextMessage("Hello world!");
+                messageProducer.send(message, DeliveryMode.NON_PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
+            }
+
+            if(CONSUMER) {
+                if(USE_ONMESSAGE) {
+                    CountDownLatch latch = new CountDownLatch(1);
+                    messageConsumer.setMessageListener(new MessageListener() {
+
+                        @Override
+                        public void onMessage(Message message) {
+                            try {
+                                Thread.sleep(100);
+                                String body = message.getBody(String.class);
+                                System.out.println(body);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            } finally {
+                                latch.countDown();
+                            }
+                        }
+                    });
+
+                    if(!latch.await(TIMEOUT, TimeUnit.MILLISECONDS)) {
+                        System.out.println("No message received within the given timeout!");
+                    }
+                } else {
+                    Thread.sleep(100);
+
+                    TextMessage receivedMessage = (TextMessage) messageConsumer.receive(TIMEOUT);
+
+                    if (receivedMessage != null) {
+                        System.out.println(receivedMessage.getText());
+                    } else {
+                        System.out.println("No message received within the given timeout!");
+                    }
+                }
             }
 
             connection.close();
@@ -70,6 +159,7 @@ public class HelloWorld {
             exp.printStackTrace(System.out);
             System.exit(1);
         }
+
     }
 
     private static class MyExceptionListener implements ExceptionListener {
@@ -79,5 +169,18 @@ public class HelloWorld {
             exception.printStackTrace(System.out);
             System.exit(1);
         }
+    }
+
+    private static void doHackyTestingConfiguration(Configuration configuration) {
+        // Everything below would normally be configured remotely or by env variable, hack here for testing only
+        SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv();
+        samplerConfig.withType("const");
+        samplerConfig.withParam(1);
+
+        ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv();
+        reporterConfig.withLogSpans(true);
+
+        configuration.withReporter(reporterConfig);
+        configuration.withSampler(samplerConfig);
     }
 }
